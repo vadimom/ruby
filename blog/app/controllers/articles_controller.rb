278c3f451d2216d25564f9d084d333d3605af5e5
Vadim Omeltchenko <vadim_omeltchenko@yahoo.com>
2015-02-06 20:22:23 -0500

    tweaks

---
+++ blog/app/controllers/articles_controller.rb
@@ -0,0 +1,13 @@
+class ArticlesController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @article = Article.new(params[:article])
+
+    @article.save
+    redirect_to @article
+  end
+
+end
